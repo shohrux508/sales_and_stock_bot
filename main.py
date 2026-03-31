b26f243203b795d36324d15dc5a488706008ec59
shohrux508 <yigitalievshohrux@gmail.com>
2026-04-01 03:30:40 +0500

    ...

--- main.py
+++ main.py
@@ -24,4 +24,5 @@ if __name__ == "__main__":
              asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
         asyncio.run(main())
     except KeyboardInterrupt:
+        
         pass
